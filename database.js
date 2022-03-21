b5c1ab22bcf17eca099a2b5431b3bd98a341f19c
lucasnpc <lucasnpc2@outlook.com>
2022-03-21 20:04:53 -0300

    Remove remote Connection because Heroku connection Doesnt work

--- database.js
+++ database.js
@@ -1,12 +1,11 @@
 const { Client } = require('pg');
 
 const client = new Client({
-    user: 'xjmgnjsxxvmind',
-    host: 'ec2-34-236-87-247.compute-1.amazonaws.com',
-    database: 'df52t4ke2v04kt',
-    password: '63f79f22a9456c62347ca68b8c2ec8f7ffe87eec4433bb570506ddd261f98aae',
-    port: 5432,
-    ssl: { rejectUnauthorized: false }
+    user: 'postgres',
+    host: '127.0.0.1',
+    database: 'DBRestaurantesPizzarias',
+    password: '0211*',
+    port: 5432
 });
 
 client.connect();
